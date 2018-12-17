bd6bc4168b31049063000dbe05e13dba1322368e
nramabad <nramabad@gmail.com>
2018-12-17 15:10:01 -0800

    Revert song attribute name to title

--- app/models/song.rb
+++ app/models/song.rb
@@ -1,7 +1,7 @@
 
 class Song < ApplicationRecord
 
-  validates :name, :album_id, presence: true
+  validates :title, :duration, :album_id, presence: true
 
   belongs_to :album
   has_one :artist, through: :album, source: :artist
